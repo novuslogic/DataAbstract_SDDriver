4694a3ede654169b29dea0fffe2bfd1da329f25e
novuslogic <support@novuslogic.com>
2015-03-15 21:58:09 +1100

    Update 1.0.8 Fixes for ADO

---
+++ Source/1.0.8/DASDDrv.dpr
@@ -0,0 +1,14 @@
+library DASDDrv;
+
+uses
+  ShareMem,
+  uDASDDriver in 'uDASDDriver.pas',
+  uDASQLDirUtils in 'uDASQLDirUtils.pas';
+
+{$E dad}
+
+{$R *.res}
+
+
+begin
+end.
---
+++ Source/1.0.8/DASDDrv_D18.dpr
@@ -0,0 +1,14 @@
+library DASDDrv_D18;
+
+uses
+  ShareMem,
+  uDASDDriver in 'uDASDDriver.pas',
+  uDASQLDirUtils in 'uDASQLDirUtils.pas';
+
+{$E dad}
+
+{$R *.res}
+
+
+begin
+end.
